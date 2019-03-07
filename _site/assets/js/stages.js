b0d9b81a5661a8b13c9d95930d56544fae4ba59c
Tiffany VACHEZ <tiffany.vachez@toucantoco.com>
2019-03-08 00:37:09 +0100

    add vuejs

--- _site/assets/js/stages.js
+++ _site/assets/js/stages.js
@@ -4,11 +4,26 @@ var app = new Vue({
   data: function() {
     return {
       message: 'Hello Vue!',
-      stages: null
+      stages: null,
+      filterKey: 'all'
+    }
+  },
+  computed: {
+    filteredStages: function() {
+      return this[this.filterKey]
+    },
+    all: function() {
+      return this.stages
+    },
+    level1: function() {
+      return _.filter(this.stages, {level: "1"});
+    },
+    level2: function() {
+      return _.filter(this.stages, {level: "2"});
     }
   },
   methods: {
-    getStages: function () {
+    getStages: function() {
       var self = this;
       var baseUrl = window.location.origin;
       $.getJSON(baseUrl + '/assets/json/stages.json', function (stages) {
